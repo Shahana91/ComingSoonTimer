29e7ed0a448440f42a8220f914840ac01dd55a48
Shahana91 <shirinsam05@gmail.com>
2024-01-29 11:28:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//Setup End Date for Countdown
+let launchDate=new Date('Feb 5,2024 12:00:00').getTime();
+//Setup Timer to tick every 1 second
+let timer=setInterval(tick,1000);
+
+function tick(){
+    //Get current time
+    let now=new Date().getTime();
+    //Get the difference in time to get time left until reaches 0
+    let t=launchDate-now;
+    //Check if time is above 0
+    if(t>0){
+        //Setup days, hours, seconds and minutes
+
+        //Algorithm to calculate days....
+        let days=Math.floor(t/(1000*60*60*24));
+
+        if(days<10){
+            days='0'+ days;
+        }
+
+        //Algorithm to calculate hours....
+        let hours=Math.floor(t%(1000*60*60*24)/(1000*60*60));
+        if(hours<10){
+            hours='0'+ hours;
+        }
+
+        //Algorithm to calculate minutes....
+        let mins=Math.floor(t%(1000*60*60)/(1000*60));
+        if(mins<10){
+            mins='0'+ mins;
+        }
+
+        //Algorithm to calculate minutes....    
+        let secs=Math.floor(t%(1000*60)/(1000));
+        if(secs<10){
+            secs='0'+ secs;
+        }
+
+        //Create Time String
+        // let time=`${days} days : ${hours} hrs : ${mins} mins : ${secs} secs`
+        let time=`${days} : ${hours} : ${mins} : ${secs}`
+
+        //Set time on document
+        document.querySelector('.countdown').innerText=time;
+
+
+    }
+
+}
